36caa5778f6597b3d4c84476d19681a07c6ddcd2
Corey H <c.l.halicke@gmail.com>
2019-07-22 18:00:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,6 +20,8 @@ class Transfer
       sender.balance -= @amount
       receiver.balance += @amount
       @status = "complete"
+    else
+      @status = "Transaction rejected. Please check your account balance."
     end
   end
 end
