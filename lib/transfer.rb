1173dc28a70c3da7d6fdb14be5d31696d1c5e6ae
Corey H <c.l.halicke@gmail.com>
2019-07-22 17:50:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,6 @@ class Transfer
   end
 
   def execute_transaction
-    binding.pry
     if self.valid? && @status == "pending" && @@transaction != self
       @@transaction = []
       sender.balance -= @amount
